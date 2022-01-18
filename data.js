c50ba72513bd595c6e80792705110cd24e9c2728
alokkumar-dev <alokshakya2601@gmail.com>
2022-01-18 19:09:25 +0530

    modilfy files

---
+++ data.js
@@ -0,0 +1,235 @@
+const newArrivalObj_1 =[
+    {
+        image: "https://image.uniqlo.com/UQ/ST3/in/imagesgoods/445293/item/ingoods_64_445293.jpg?width=1008&impolicy=quality_75",
+        gendar: "WOMEN",
+        size: "S-3XL",
+        description: "WOMEN Extra Stretch High Rise Denim Leggings Trousers",
+        exclusive: "",
+        price: "1,990.00"
+    },
+    {
+        image: "https://image.uniqlo.com/UQ/ST3/in/imagesgoods/433604/item/ingoods_30_433604.jpg?width=1008&impolicy=quality_75",
+        gendar: "WOMEN",
+        size: "XS-XXL",
+        description: "WOMEN Rayon Long Sleeve Blouse",
+        exclusive: "Exclusive Size Online Only",
+        price: "1,990.00"
+    },
+   
+    {
+        image: "https://image.uniqlo.com/UQ/ST3/in/imagesgoods/445292/item/ingoods_56_445292.jpg?width=1008&impolicy=quality_75",
+        gendar: "WOMEN",
+        size: "S-3XL",
+        description: "WOMEN Extra Stretch High Rise Denim Leggings Trousers",
+        exclusive: "Exclusive Size Online Only\n New Arrivals",
+        price: "1,990.00"
+    },
+    {
+        image: "https://image.uniqlo.com/UQ/ST3/in/imagesgoods/445388/item/ingoods_12_445388.jpg?width=1008&impolicy=quality_75",
+        gendar: "WOMEN",
+        size: "XS-XXL",
+        description: "WOMEN Wirless Bra Active Square Neck",
+        exclusive: "",
+        price: "1,990.00"
+    }
+  
+];
+
+const newArrivalObj_2 =[
+    {
+        image: "https://image.uniqlo.com/UQ/ST3/in/imagesgoods/439088/item/ingoods_34_439088.jpg?width=1008&impolicy=quality_75",
+        gendar: "WOMEN",
+        size: "XS-XXL",
+        description: "WOMEN Soft Pile Lined Sweat Long Sleeve Full-Zip Hoodie",
+        exclusive: "Exclusive Size Online Only",
+        price: "1,990.00"
+    },
+    {
+        image: "https://image.uniqlo.com/UQ/ST3/in/imagesgoods/439093/item/ingoods_09_439093.jpg?width=1008&impolicy=quality_75",
+        gendar: "WOMEN",
+        size: "S-XXL",
+        description: "WOMEN HEATTECH Pile Lined Sweat Trousers",
+        exclusive: "Exclusive Size Online Only",
+        price: "2,490.00"
+    },
+    {
+        image: "https://image.uniqlo.com/UQ/ST3/in/imagesgoods/439702/item/ingoods_74_439702.jpg?width=1008&impolicy=quality_75",
+        gendar: "WOMEN",
+        size: "S-3XL",
+        description: "WOMEN Ultra Light down Jecket",
+        exclusive: "Exclusive Size Online Only",
+        price: "4,990.00"
+    },
+    {
+        image: "https://image.uniqlo.com/UQ/ST3/in/imagesgoods/439532/item/ingoods_78_439532.jpg?width=1008&impolicy=quality_75",
+        gendar: "WOMEN",
+        size: "XS-XL",
+        description: "WOMEN HEATTECH Bra Sleeveless Top (Regular Warm) Thermal",
+        exclusive: "Exclusive Size Online Only",
+        price: "1,990.00"
+    },
+  
+];
+
+const newArrivalObj_3 =[
+    {
+        image: "https://image.uniqlo.com/UQ/ST3/in/imagesgoods/441156/item/ingoods_56_441156.jpg?width=1008&impolicy=quality_75",
+        gendar: "WOMEN",
+        size: "XS-XXL",
+        description: "WOMEN Low Gauge Sweater",
+        exclusive: "Exclusive Size Online Only",
+        price: "1,990.00"
+    },
+    {
+        image: "https://image.uniqlo.com/UQ/ST3/in/imagesgoods/439161/item/ingoods_01_439161.jpg?width=1008&impolicy=quality_75",
+        gendar: "WOMEN",
+        size: "XS-XL",
+        description: "WOMEN HEATTECH Pile Lined Sweat Trousers",
+        exclusive: "Exclusive Size Online Only",
+        price: "2,490.00"
+    },
+    {
+        image: "https://image.uniqlo.com/UQ/ST3/in/imagesgoods/447916/item/ingoods_37_447916.jpg?width=1008&impolicy=quality_75",
+        gendar: "WOMEN",
+        size: "S-3XL",
+        description: "WOMEN Cotton Wide Relax",
+        exclusive: "Online Only",
+        price: "2,990.00"
+    },
+    {
+        image: "https://image.uniqlo.com/UQ/ST3/in/imagesgoods/443915/item/ingoods_16_443915.jpg?width=1008&impolicy=quality_75",
+        gendar: "WOMEN",
+        size: "S-L",
+        description: "WOMEN Flannel Shirt (Long Sleeve)",
+        exclusive: "Online Only",
+        price: "2,490.00"
+    },
+  
+];
+
+const newArrivalObj_4 =[
+    {
+        image: "https://image.uniqlo.com/UQ/ST3/in/imagesgoods/448440/item/ingoods_63_448440.jpg?width=1008&impolicy=quality_75",
+        gendar: "WOMEN",
+        size: "XS-XXL",
+        description: "WOMEN Cotton Stand Collar Long Sleeve Shirt",
+        exclusive: "Exclusive Size Online Only",
+        price: "2,490.00"
+    },
+    {
+        image: "https://image.uniqlo.com/UQ/ST3/in/imagesgoods/445292/item/ingoods_56_445292.jpg?width=1008&impolicy=quality_75",
+        gendar: "WOMEN",
+        size: "S-3XL",
+        description: "WOMEN Extra Stretch High Rise Leggings Trousers",
+        exclusive: "",
+        price: "1,990.00"
+    },
+    {
+        image: "https://image.uniqlo.com/UQ/ST3/in/imagesgoods/448223/item/ingoods_00_448223.jpg?width=1008&impolicy=quality_75",
+        gendar: "WOMEN",
+        size: "XS-XXL",
+        description: "WOMEN Raron Dolman 3/4 Sleeve Blouse",
+        exclusive: "Exclusive Size Online Only",
+        price: "1,990.00"
+    },
+    {
+        image: "https://image.uniqlo.com/UQ/ST3/in/imagesgoods/439473/item/ingoods_34_439473.jpg?width=1008&impolicy=quality_75",
+        gendar: "WOMEN",
+        size: "S-3XL",
+        description: "WOMEN Cotton Jogger Trousers",
+        exclusive: "Exclusive Size Online Only",
+        price: "2,490.00"
+    },
+  
+];
+
+const newArrivalObj_5 =[
+    {
+        image: "https://im.uniqlo.com/global-cms/spa/res51e18e31fb2439e0c7dc3c13924ea8aefr.jpg",
+    },
+    {
+        image: "https://im.uniqlo.com/global-cms/spa/res1f10e73682218e376c6f9bffd6bda254fr.jpg",
+    },
+    {
+        image: "https://im.uniqlo.com/global-cms/spa/res93036e3545b87a3a557e7f570e55e0bdfr.jpg",
+    },
+    {
+        image: "https://im.uniqlo.com/global-cms/spa/resfbc38b913d6e1b477a6ff67dd2f1ff96fr.jpg",
+    },
+    {
+        image: "https://im.uniqlo.com/global-cms/spa/res6cc2b1babcf722629f11049d6400fea3fr.jpg",
+    },
+    {
+        image: "https://im.uniqlo.com/global-cms/spa/resa878e2a8565de26b3baa684278ad37effr.jpg",
+    },
+    {
+        image: "https://im.uniqlo.com/global-cms/spa/res8301fcb852e891e1a0b3c9864c67b4a9fr.jpg",
+    },
+    {
+        image: "https://im.uniqlo.com/global-cms/spa/res7425ad492d279378d27a956ab1a7312dfr.jpg",
+    },
+    {
+        image: "https://im.uniqlo.com/global-cms/spa/res2c87205dc1d06ef92c4fe200ce02baa0fr.jpg",
+    },
+  
+];
+
+const newArrivalObj_6 =[
+    {
+        image: "https://im.uniqlo.com/global-cms/spa/resca7228f636e1f9e9983d8be65e5b5145fr.jpg",
+        exclusive: "ULTRA LIGHT DOWN COLLECTION",
+        description: "A new essential for your new way of  life.",
+    },
+    {
+        image: "https://im.uniqlo.com/global-cms/spa/res2e98846b0ba4832ff9a67ecad4542c79fr.jpg",
+        exclusive: "FLEECE COLLECTION",
+        description: "Express your personality and stay cozy all seasonlong - just reach for your favorite fleece.",
+    },
+    {
+        image: "https://im.uniqlo.com/global-cms/spa/resc8b66447eb9ae8c8f74035c5f62762b8fr.jpg",
+        exclusive: "HEATTECH COLLECTION",
+        description: "Wear less, More Warmth. Innovative fabric generates heat from your body moisture.",
+    },
+    
+  
+];
+
+const newArrivalObj_7 =[
+    {
+        image: "https://im.uniqlo.com/global-cms/spa/resdef97f92047f03ca94ebda796a312568fr.jpg",
+        exclusive: "ULTRA LIGHT DOWN COLLECTION",
+        description: "A new essential for your new way of  life.",
+    },
+    {
+        image: "https://im.uniqlo.com/global-cms/spa/resa3b0a61ef92c82820124a327ecd34303fr.jpg",
+        exclusive: "FLEECE COLLECTION",
+        description: "Express your personality and stay cozy all seasonlong - just reach for your favorite fleece.",
+    },
+    {
+        image: "https://im.uniqlo.com/global-cms/spa/res1e5990de62ff4e0a54c912cd13fc1f21fr.jpg",
+        exclusive: "HEATTECH COLLECTION",
+        description: "Wear less, More Warmth. Innovative fabric generates heat from your body moisture.",
+    },
+    
+  
+];
+
+const newArrivalObj_8 =[
+    {
+        image: "https://im.uniqlo.com/global-cms/spa/resdef97f92047f03ca94ebda796a312568fr.jpg",
+        exclusive: "ULTRA LIGHT DOWN COLLECTION",
+        description: "A new essential for your new way of  life.",
+    },
+    {
+        image: "https://im.uniqlo.com/global-cms/spa/resa3b0a61ef92c82820124a327ecd34303fr.jpg",
+        exclusive: "FLEECE COLLECTION",
+        description: "Express your personality and stay cozy all seasonlong - just reach for your favorite fleece.",
+    },
+    {
+        image: "https://im.uniqlo.com/global-cms/spa/res1e5990de62ff4e0a54c912cd13fc1f21fr.jpg",
+        exclusive: "HEATTECH COLLECTION",
+        description: "Wear less, More Warmth. Innovative fabric generates heat from your body moisture.",
+    },
+    
+  
+];
