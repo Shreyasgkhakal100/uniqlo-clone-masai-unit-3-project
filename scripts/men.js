c1485c3f8ed75ca50d70a9d76a80ae7e8a676ddb
Sudhanshu Kumar <sudhanshuk384@gmail.com>
2022-01-18 23:38:05 +0530

    somechange

--- scripts/men.js
+++ scripts/men.js
@@ -159,3 +159,236 @@ function limitedOffers(data){
     limited_off.append(div)
   })
 }
+
+
+let newMenObj3= [
+  {
+    image : "https://image.uniqlo.com/UQ/ST3/AsianCommon/imagesgoods/446519/item/goods_69_446519.jpg?width=1008&impolicy=quality_75",
+    gender : "MEN",
+    size  :  "XS-XXL",
+    name :  "MEN Easy Work Trousers",
+    exc :  "Exclusive Size Online Only",
+    striked_price : "Rs. 4,990.00",
+    price : "Rs. 3,990.00",
+    limited : "Sale",
+    star : "⭐(1)",
+  },
+  {
+    image : "https://image.uniqlo.com/UQ/ST3/in/imagesgoods/448661/item/ingoods_01_448661.jpg?width=1008&impolicy=quality_75",
+    gender : "MEN",
+    size  :  "S-3XL",
+    name :  "MEN Smart Ankle Trousers 2WAY Stretch Cotton",
+    exc : "Exclusive Size Online Only,New Arrivals",
+    striked_price :"Rs. 2,490.00",
+    price : "Rs. 1,990.00",
+    limited : "Sale",
+    star : "⭐⭐⭐⭐⭐(2)",
+  },
+  {
+    image : "https://image.uniqlo.com/UQ/ST3/in/imagesgoods/438793/item/ingoods_69_438793.jpg?width=1008&impolicy=quality_75",
+    gender : "MEN",
+    size  :  "S-3XL",
+    name : "MEN Souffle Yarn Mock Neck Long Sleeve Sweater", 
+    exc : "Exclusive Size Online Only",
+    striked_price :"Rs. 2,490.00",
+    price : "Rs. 1,990.00",
+    limited :"Sale",
+    star : "⭐⭐⭐⭐(2)",
+  },
+  {
+    image : "https://image.uniqlo.com/UQ/ST3/AsianCommon/imagesgoods/446430/item/goods_08_446430.jpg?width=1008&impolicy=quality_75",
+    gender : "MEN",
+    size  :  "S-L",
+    name :  "MEN Supima Cotton Oversized Stand Collar Shirt",
+    exc : "Online Only",
+    striked_price : "Rs. 2,990.00",
+    price : "Rs. 1,990.00",
+    limited : "Sale",
+    star : "",
+  },
+]
+ let sale =document.getElementById("sale_product")
+ SaleOffers(newMenObj3)
+function  SaleOffers(data){
+
+  data.map(({image,gender,size,name,exc,striked_price,price,limited,star})=>{
+    let div =document.createElement("div");
+    let imgdiv = document.createElement("div");
+    let sizediv =document.createElement("div");   
+    let img =document.createElement("img");
+    img.src =image;
+
+    let gend = document.createElement("h4");
+    gend.textContent =gender;
+    let siz = document.createElement("h4");
+    siz.textContent =size;
+
+    let nam = document.createElement("h3");
+    nam.textContent =name;
+
+    let des = document.createElement("p");
+    des.textContent =exc;
+    des.id ="desc"
+
+    let striked = document.createElement("p");
+    striked.textContent =striked_price;
+    striked.id ="strikedprice"
+    let pric = document.createElement("p");
+    pric.textContent =price;
+    pric.id ="redprice"
+
+    let limit = document.createElement("p");
+    limit.textContent =limited;
+    limit.style.color ="red"
+       
+    let str = document.createElement("p");
+    str.textContent =star;
+    // str.style.border ="0"
+    imgdiv.append(img);
+    sizediv.append(gend,siz);
+    sizediv.id = "men"
+    div.append(imgdiv,sizediv,nam,des,striked,pric,limit,str);
+    sale.append(div)
+  })
+}
+
+
+let newMenObj4=[
+  {
+      image : "https://image.uniqlo.com/UQ/ST3/in/imagesgoods/444553/item/ingoods_67_444553.jpg?width=1008&impolicy=quality_75",
+      gender : "MEN",
+      size  : "S-XXL",
+      name : "MEN Utility Parka",
+      desc : "Limited Store",
+      price : "Rs. 4,990.00",
+      star : "⭐⭐⭐⭐⭐(1)",
+  },
+  {
+    image : "https://image.uniqlo.com/UQ/ST3/in/imagesgoods/440522/item/ingoods_66_440522.jpg?width=1008&impolicy=quality_75",
+    gender : "MEN",
+    size  : "S-3XL",
+    name : "Waffle Crew Neck Long Sleeve T-Shirt",
+    desc : "Exclusive Size Online Only",
+    price : "Rs. 1,990.00",
+    star : "⭐⭐⭐⭐⭐(3)",
+  },
+  {
+    image : "https://image.uniqlo.com/UQ/ST3/in/imagesgoods/444583/item/ingoods_05_444583.jpg?width=1008&impolicy=quality_75",
+    gender : "MEN",
+    size  : "28INCH-38INCH",
+    name : "MEN Extra Stretch Skinny Fit Jeans",
+    desc : "Exclusive Size Online Only",
+    price : "Rs. 2,990.00",
+    star : "⭐⭐⭐⭐⭐(1)",
+  },
+  {
+    image : "https://image.uniqlo.com/UQ/ST3/in/imagesgoods/448134/item/ingoods_04_448134.jpg?width=1008&impolicy=quality_75",
+    gender : "MEN",
+    size  : "S-3XL",
+    name : "MEN Ultra Stretch Dry Sweat Trousers",
+    desc : "Exclusive Size Online Only,New Arrivals",
+    price : "Rs. 2,490.00",
+    star : "⭐⭐⭐⭐⭐(5)",
+  },
+]
+
+let feature =document.getElementById("featured_offer")
+featureprod(newMenObj4)
+function featureprod(data){
+
+  data.map(({image,gender,size,name,desc,price,star})=>{
+     let div =document.createElement("div");
+     let imgdiv = document.createElement("div");
+     let sizediv =document.createElement("div");   
+     let img =document.createElement("img");
+     img.src =image;
+
+     let gend = document.createElement("h4");
+     gend.textContent =gender;
+     let siz = document.createElement("h4");
+     siz.textContent =size;
+
+     let nam = document.createElement("h3");
+     nam.textContent =name;
+
+     let des = document.createElement("h3");
+     des.textContent =desc;
+     des.id ="desc"
+
+     let pric = document.createElement("h4");
+     pric.textContent =price;
+
+     let str = document.createElement("h4");
+     str.textContent =star;
+
+     imgdiv.append(img);
+     sizediv.append(gend,siz);
+     sizediv.id = "men"
+    // sizediv.style.display ="flex"
+
+
+     div.append(imgdiv,sizediv,nam,des,pric,str)
+     feature.append(div)
+  })
+}
+let pickupimg =[
+  {
+    image : "https://im.uniqlo.com/global-cms/spa/res8d80b49276715e269314f640dc787af7fr.jpg",
+  },
+  {
+    image : "https://im.uniqlo.com/global-cms/spa/resb017b915e0496d3f006995675f290d86fr.jpg",
+  },
+  {
+    image : "https://im.uniqlo.com/global-cms/spa/rese86928c1ccc41b21e28feeb046b35bd1fr.jpg",
+  },
+  {
+    image : "https://im.uniqlo.com/global-cms/spa/res9c1cfa476bbfd0e0886a79738894c8a3fr.jpg"
+  },
+  {
+    image :"https://im.uniqlo.com/global-cms/spa/res3d3695458dbf64a866666af18a51d948fr.jpg"
+  },
+  {
+    image : "https://im.uniqlo.com/global-cms/spa/res56abd7a444e2e4077a5d0193e0a07e52fr.jpg"
+  },
+  {
+    image : "https://im.uniqlo.com/global-cms/spa/res8caeee0cec0c5885a8fa1edb836fa95cfr.jpg"
+  },
+  {
+    image : "https://im.uniqlo.com/global-cms/spa/resb396bd6cdd7368d6a2d8dbabb350266efr.jpg"
+  },
+  {
+    image : "https://im.uniqlo.com/global-cms/spa/resb62383efa267630e25929bcbcf086bf2fr.jpg"
+  }
+]
+let pickup =document.getElementById("pickup");
+pickupCAtegory(pickupimg)
+function pickupCAtegory(data){
+   data.map(({image})=>{
+     let div =document.createElement("div")
+     let img = document.createElement("img");
+     img.src =image
+     div.append(img)
+     pickup.append(div)
+   })
+} 
+
+let featuredobj =[
+  {
+    image : "https://im.uniqlo.com/global-cms/spa/res5efd0925742055f25d72b0ac617cf7c0fr.jpg"
+  },
+  // {
+  //   image : "https://im.uniqlo.com/global-cms/spa/res5efd0925742055f25d72b0ac617cf7c0fr.jpg"
+  // },
+  // {
+  //   image : "https://im.uniqlo.com/global-cms/spa/res5efd0925742055f25d72b0ac617cf7c0fr.jpg"
+  // },
+]
+ let featurenews =document.getElementById("featured_news");
+ featurednews(featuredobj)
+function featurednews(data){
+   data.map(({image})=>{
+     let img =document.createElement("img");
+     img.src =image;
+     featurenews.append(img)
+   })
+}
