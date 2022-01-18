355ba6ae9f8dd2495db4cbd4fa6fbe95f91dce40
Shreyas Khakal <91959869+Shreyasgkhakal100@users.noreply.github.com>
2022-01-18 22:26:01 +0530

    Commit

---
+++ scripts/cart.js
@@ -0,0 +1,75 @@
+const recentlyViewObj = [
+  {
+    image:
+      "https://image.uniqlo.com/UQ/ST3/in/imagesgoods/445293/item/ingoods_64_445293.jpg?width=1008&impolicy=quality_75",
+    gendar: "WOMEN",
+    size: "S-3XL",
+    description: "WOMEN Extra Stretch High Rise Denim Leggings Trousers",
+    exclusive: "",
+    price: "1,990.00",
+  },
+  {
+    image:
+      "https://image.uniqlo.com/UQ/ST3/in/imagesgoods/433604/item/ingoods_30_433604.jpg?width=1008&impolicy=quality_75",
+    gendar: "WOMEN",
+    size: "XS-XXL",
+    description: "WOMEN Rayon Long Sleeve Blouse",
+    exclusive: "Exclusive Size Online Only",
+    price: "1,990.00",
+  },
+
+  {
+    image:
+      "https://image.uniqlo.com/UQ/ST3/in/imagesgoods/445292/item/ingoods_56_445292.jpg?width=1008&impolicy=quality_75",
+    gendar: "WOMEN",
+    size: "S-3XL",
+    description: "WOMEN Extra Stretch High Rise Denim Leggings Trousers",
+    exclusive: "Exclusive Size Online Only\n New Arrivals",
+    price: "1,990.00",
+  },
+  {
+    image:
+      "https://image.uniqlo.com/UQ/ST3/in/imagesgoods/445388/item/ingoods_12_445388.jpg?width=1008&impolicy=quality_75",
+    gendar: "WOMEN",
+    size: "XS-XXL",
+    description: "WOMEN Wirless Bra Active Square Neck",
+    exclusive: "",
+    price: "1,990.00",
+  },
+];
+
+
+
+
+recentlyView(recentlyViewObj);
+function recentlyView(data) {
+  data.map(({ image, gendar, size, description, exclusive, price }) => {
+    let mainDiv = document.createElement("div");
+    let imgDiv = document.createElement("div");
+    imgDiv.setAttribute("class", "imgDiv");
+    let sizeDiv = document.createElement("div");
+    sizeDiv.setAttribute("class", "sizeDiv");
+
+    let img = document.createElement("img");
+    let gend = document.createElement("h3");
+    let siz = document.createElement("h3");
+    let desc = document.createElement("h2");
+    let exclus = document.createElement("h4");
+    let pric = document.createElement("h4");
+
+    exclus.setAttribute("class", "exclusive");
+
+    img.src = image;
+    gend.innerText = gendar;
+    siz.innerText = size;
+    desc.innerText = description;
+    exclus.innerText = exclusive;
+    pric.innerText = `Rs. ${price}`;
+
+    imgDiv.append(img);
+    sizeDiv.append(gend, siz);
+    mainDiv.append(imgDiv, sizeDiv, desc, exclus, pric);
+
+    document.querySelector(".slideProducts").append(mainDiv);
+  });
+}
