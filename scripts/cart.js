431d55aa9a49fbfd713c701bb11e893bae6dfcb9
Shreyas Khakal <91959869+Shreyasgkhakal100@users.noreply.github.com>
2022-01-19 19:59:11 +0530

    commit

--- scripts/cart.js
+++ scripts/cart.js
@@ -40,7 +40,6 @@ const recentlyViewObj = [
 
 
 
-
 recentlyView(recentlyViewObj);
 function recentlyView(data) {
   data.map(({ image, gendar, size, description, exclusive, price }) => {
@@ -72,4 +71,66 @@ function recentlyView(data) {
 
     document.querySelector(".slideProducts").append(mainDiv);
   });
+}
+
+
+//cart div appending
+
+
+let cart = JSON.parse(localStorage.getItem("cart")) || [
+  {
+    image:
+      "https://image.uniqlo.com/UQ/ST3/in/imagesgoods/445292/item/ingoods_56_445292.jpg?width=1008&impolicy=quality_75",
+    gendar: "WOMEN",
+    size: "S-3XL",
+    description: "WOMEN Extra Stretch High Rise Denim Leggings Trousers",
+    exclusive: "Exclusive Size Online Only\n New Arrivals",
+    price: "1,990.00",
+  },
+  {
+    image:
+      "https://image.uniqlo.com/UQ/ST3/in/imagesgoods/445388/item/ingoods_12_445388.jpg?width=1008&impolicy=quality_75",
+    gendar: "WOMEN",
+    size: "XS-XXL",
+    description: "WOMEN Wirless Bra Active Square Neck",
+    exclusive: "",
+    price: "1,990.00",
+  },
+];
+
+let cartDiv = document.querySelector("#cartDiv");
+if(cart.length>0){
+  showCart(cart);
+}
+function showCart(cart){
+  cartDiv.innerHTML = "";
+  let showCartDiv = document.createElement("div");
+  showCartDiv.setAttribute("class", "showCartDiv");
+
+    let leftDiv = document.createElement("div");
+    leftDiv.setAttribute("class", "leftDiv");
+    let rightDiv = document.createElement("div");
+    rightDiv.setAttribute("class", "rightDiv");
+
+    showCartDiv.append(leftDiv, rightDiv);
+    cartDiv.append(showCartDiv);
+
+    cart.map(({ image, gendar, size, description, exclusive, price }) => {
+      let imgDivCart = document.createElement("div");
+      imgDivCart.setAttribute("class", "imgDivCart");
+      let imgCart = document.createElement("img");
+      imgCart.src = image;
+
+      let infoDivCart = document.createElement("div");
+      infoDivCart.setAttribute("class", "infoDivCart");
+
+      let titleAndCancel = document.createElement("div");
+      let title = document.createElement("h2");
+      title.innerHTML = description;
+
+      let cancel = document.createElement("h2");
+      cancel.innerHTML = "&times;";
+    });
+    
+
 }
