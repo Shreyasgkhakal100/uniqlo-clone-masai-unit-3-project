2fc7cb990d7f45385a3690410e3e0b65f4e11977
srvsumanss <srvsumanss@gmail.com>
2022-01-23 11:34:07 +0530

    final

--- scripts/import.js
+++ scripts/import.js
@@ -1,10 +1,10 @@
 let navdiv = document.getElementById("navbar")
 let footerdiv = document.getElementById("footer")
 
-console.log("FOOTER",footerdiv ,"Navbar",navbar)
+console.log("FOOTER",footerdiv )
 
- import {navbar} from "../components/navbar_full.js"
-navdiv.innerHTML = navbar()
+//  import {navbar} from "../components/navbar_full.js"
+// navdiv.innerHTML = navbar()
 
 import footer from "../components/footer.js"
 footerdiv.innerHTML = footer()
