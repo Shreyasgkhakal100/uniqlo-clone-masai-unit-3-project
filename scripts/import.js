1c4b17a3d33ad7b40a1e65e57502055662ecb854
srvsumanss <srvsumanss@gmail.com>
2022-01-23 10:45:56 +0530

    Maza Maa

--- scripts/import.js
+++ scripts/import.js
@@ -1,10 +1,10 @@
 let navdiv = document.getElementById("navbar")
 let footerdiv = document.getElementById("footer")
 
-console.log("FOOTER",footerdiv)
+console.log("FOOTER",footerdiv ,"Navbar",navbar)
 
-// import {navbar} from "../components/navbar.js"
-// navdiv.innerHTML = navbar()
+ import {navbar} from "../components/navbar_full.js"
+navdiv.innerHTML = navbar()
 
 import footer from "../components/footer.js"
 footerdiv.innerHTML = footer()
--- scripts/login.js
+++ scripts/login.js
@@ -4,7 +4,8 @@ function signin(e) {
     let password = document.getElementById("password").value;
 
     var userData = JSON.parse(localStorage.getItem("User")) || [];
-
+    let userStatus = false;
+    localStorage.setItem("userStatus", userStatus);
     let all_users = JSON.parse(localStorage.getItem("userDataBase"));
     let flag = true ;
     all_users.forEach(function (user) {
@@ -13,7 +14,8 @@ function signin(e) {
        {
            flag=false
         alert("Logged in Succesfully");
-
+        userStatus = true;
+        localStorage.setItem("userStatus", userStatus);
         window.location.href = "profile.html";       // enter location where you want to redirect after successful login
         localStorage.setItem("User",JSON.stringify(user));
         console.log("User:",user)
@@ -21,6 +23,8 @@ function signin(e) {
 
     });
     if(flag){
+      userStatus = false;
+      localStorage.setItem("userStatus", userStatus);
         alert("Invalid Email Id or Password")
     }
   }
